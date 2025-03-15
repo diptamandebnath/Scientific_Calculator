38eb977c049bcb27be40183c6cd8770f43fe425e
Diptaman Debnath <diptaman9999@gmail.com>
2025-03-15 16:18:26 +0530

    Commited

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF1D1D1D);
+  static const Color panelBackground = Color(0xFF222222);
+  static const Color displayText = Color(0xFFE34208);
+  static const Color resultText = Color(0xFFFFFFFF);
+  static const Color buttonBackground = Color(0xFF065393);
+  static const Color buttonBorder = Color(0xFFE6A70A);
+  static const Color buttonText = Color(0xFFFFFFFF);
+  static const Color pink =
+      Color(0xFFFF4081); // Highlight color for specific buttons
+}
---
+++ lib/widgets/display.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+
+class Display extends StatelessWidget {
+  final String expression;
+  final String result;
+
+  const Display({super.key, required this.expression, required this.result});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            expression,
+            style: const TextStyle(fontSize: 24, color: AppColors.displayText),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "= $result",
+            style: const TextStyle(
+                fontSize: 36,
+                color: AppColors.resultText,
+                fontWeight: FontWeight.bold),
+          ),
+          Column(
+            children: [
+              Text(
+                'Developed by Students of Department of IT, Tripura University',
+                style: TextStyle(fontSize: 10,
+                  color: Colors.amber,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+  }
+}
